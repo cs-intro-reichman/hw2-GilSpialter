cf8d7323b6b5c4bc9eb89eb83563fe471df56a0f
Gil Spialter <gilspialter@MacBook-Pro.local>
2024-11-15 08:36:25 +0200

    commiting code first time

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		int denom = 1;
+		double sum = 0.0;
+		int q = 1;
+
+		for (int i = 0; i < n; i++) {
+			sum += (1.0 / denom * q);
+			denom += 2;
+			q *= (-1);
+		}
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + sum * 4);
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,30 @@
 public class TimeCalc {
+//Adds certain num of minutes to the time. Prints the new time in 24 hr format.
     public static void main(String[] args) {
-        // Replace this comment with your code
+    int HH = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+    int MM = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+
+    int inputM = Integer.parseInt(args[1]);
+    int addHr = inputM / 60;
+    int addMin = inputM % 60;
+
+    HH += addHr;
+    MM += addMin;
+    if (MM >= 60) {             //correcting hours and minutes to fit there clock:  Hours < 24,  Minutes < 60
+        MM -= 60;
+        HH ++;
+    }
+    if (HH >= 24) {
+        HH %= 24;
+    }
+                                        //Now outputting final answer.
+    if (HH < 10) {
+        System.out.print("0");
+    }
+    System.out.print(HH+":");   
+    if (MM < 10) {
+        System.out.print("0");
+    }
+    System.out.println(MM);
     }
 }
